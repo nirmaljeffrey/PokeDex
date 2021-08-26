e2053f472cac526d81c8ec1ad2f2c2b8291fa445
Nirmal Jeffrey <jeffattry@gmail.com>
2021-08-26 16:40:59 +0530

    fix: remove timber implementation

--- app/src/main/java/com/nirmaljeffrey/dev/pokedex/PokeDexApplication.kt
+++ app/src/main/java/com/nirmaljeffrey/dev/pokedex/PokeDexApplication.kt
@@ -2,12 +2,6 @@ package com.nirmaljeffrey.dev.pokedex
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-import timber.log.Timber
 
 @HiltAndroidApp
-class PokeDexApplication : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        Timber.plant(Timber.DebugTree())
-    }
-}
+class PokeDexApplication : Application()
